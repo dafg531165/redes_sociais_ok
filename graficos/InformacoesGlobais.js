1241c686cfd1447c3410eee40a77bf54f3e23008
dafg531165 <107404931+dafg531165@users.noreply.github.com>
2024-12-06 10:36:35 +0000

    .

--- graficos/InformacoesGlobais.js
+++ graficos/InformacoesGlobais.js
@@ -9,7 +9,9 @@ async function visualizarInformacoesGlobais() {
         const porcentagemConectada = ((pessoasConectadas /  pessoasNoMundo) * 100).toFixed(2);
         const paragrafo = document.createElement('p')
         paragrafo.classList.add('graficos-container__texto')
-        paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+        paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas 
+        e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma 
+        rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
         console.log(paragrafo)
         const container = document.getElementById ('graficos-container')
         container.appendChild(paragrafo)
